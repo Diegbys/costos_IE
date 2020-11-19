ffcc8fce5506ab4d2fc4d67231f1c1736a792cca
Diegbys Mudarra <diegbys.mudarra@grupoleiros.com>
2020-11-19 13:18:12 -0400

    Calcular los costos en la tabla

--- docs/js/main.js
+++ docs/js/main.js
@@ -7,13 +7,13 @@ document.addEventListener('DOMContentLoaded', function () {
 var costos = new Vue({
     el: "#costos",
     data: {
-        capacidad_Instalada: "",
+        capacidad_Instalada: 0,
         frecuencia: "",
-        margen_Beneficio: "",
+        margen_Beneficio: 0,
 
         descripcion: "",
-        costo_Fijo: "",
-        costo_Variable: "",
+        costo_Fijo: 0,
+        costo_Variable: 0,
 
         cabecera_Tabla: [
             "Descripcion",
@@ -23,24 +23,35 @@ var costos = new Vue({
         ],
 
         datos_Tabla: [],
-        costo_f_Total: "",
-        costo_v_Total: "",
-        costo_Total: "",
+        costo_f_Total: 0,
+        costo_v_Total: 0,
+        costo_Total: 0,
 
-        costo_Unitario: "",
-        precio_Venta: "",
-        costo_u_Variable: "",
-        punto_Equilibrio: "",
+        costo_Unitario: 0,
+        precio_Venta: 0,
+        costo_u_Variable: 0,
+        punto_Equilibrio: 0,
     },
     methods: {
         agregar_fila(){
             //Para agregar una fila nueva
-            let row = {descripcion: this.descripcion, costoFijo: this.costo_Fijo, costoVariable: this.costo_Variable, subtotal: (parseFloat(this.costo_Fijo) + parseFloat(this.costo_Variable)).toFixed(2)}
+            let row = {descripcion: this.descripcion, costoFijo: this.costo_Fijo, costoVariable: this.costo_Variable, subtotal: (parseFloat(this.costo_Fijo) + parseFloat(this.costo_Variable)).toFixed(3)};
             this.datos_Tabla.push(row)
+            this.descripcion = "";
+            this.costo_Fijo = "";
+            this.costo_Variable = "";
 
-            this.datos_Tabla.forEach(element => {
-                console.log(element)
+            //Marcas los totales de la tabla
+            this.costo_f_Total = 0;
+            this.costo_v_Total = 0;
+            this.costo_Total = 0;
+            this.datos_Tabla.forEach(row => {
+                console.log(row)
+                console.log(parseFloat(row.costoFijo))
+                this.costo_f_Total =  (parseFloat(this.costo_f_Total) + parseFloat(row.costoFijo)).toFixed(3);
+                this.costo_v_Total =  (parseFloat(this.costo_v_Total) + parseFloat(row.costoVariable)).toFixed(3);
             });
+            this.costo_Total =  (parseFloat(this.costo_f_Total) + parseFloat(this.costo_v_Total)).toFixed(3);
         }
     },
 })
